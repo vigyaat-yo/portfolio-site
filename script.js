3bfa1809c26286340939549ed55511cee6aaf8e6
vigyaat-yo <vigyaat26@gmail.com>
2026-03-09 07:34:49 +0530

    contact us section completed

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+// ----sheets----
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxOgwITueTC2M66SyuBN2oN6OBchI9f1eqaTA3IMbQWjS_v20ahxWO-pYhouDxL53xm/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+		.then(response => response.json())
+		.then(response => {
+            msg.innerHTML = "Message sent successfully!";
+            setTimeout(function(){
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+		.catch(error => console.error('Error!', error.message))
+})
